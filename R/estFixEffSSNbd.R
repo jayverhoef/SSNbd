5a3aa7e9de279bc135384806bafb0f1b050d108b
jay <jay.verhoef@noaa.gov>
2018-03-04 14:49:23 -0900

    initial commit

---
+++ R/estFixEffSSNbd.R
@@ -0,0 +1,79 @@
+#-------------------------------------------------------------------------------
+#
+#           estFixEffSSNbd
+#
+#-------------------------------------------------------------------------------
+
+#' estimates fixed effects with fast methods for big data with SSN
+#'
+#' estimates fixed effects with fast methods for big data with SSN
+#'
+#' @param estCovParSSN an object of class "estCovParSSNbd" created
+#'   from estimating the covariance parameters. 
+
+#'
+#' @return a list
+#'
+#' @author Jay Ver Hoef
+#' @export
+
+estFixEffSSNbd = function(estCovParSSN)
+{    
+			CorModels = as.list(estCovParSSN$mfcall[['CorModels']])
+			CorModels = CorModels[2:length(CorModels)]
+			CorModels = unlist(CorModels)
+			nSubSamp = length(estCovParSSN$distanceList)
+			Vlist = foreach(i=1:nSubSamp) %dopar% {
+      V = SSN:::makeCovMat(theta = estCovParSSN$estCovPar, 
+				dist.hydro = estCovParSSN$distanceList[[i]]$netD, 
+        a.mat = estCovParSSN$distanceList[[i]]$A, 
+        b.mat = estCovParSSN$distanceList[[i]]$B, 
+        w.matrix = estCovParSSN$distanceList[[i]]$W, 
+        net.zero = estCovParSSN$distanceList[[i]]$net0, 
+        x.row = estCovParSSN$distanceList[[i]]$xc, 
+        y.row = estCovParSSN$distanceList[[i]]$yc, 
+        x.col = estCovParSSN$distanceList[[i]]$xc, 
+        y.col= estCovParSSN$distanceList[[i]]$yc, 
+        useTailDownWeight = FALSE,
+        CorModels = CorModels, 
+        use.nugget = estCovParSSN$mfcall[['use.nugget']], 
+        use.anisotropy = FALSE, 
+        REs = estCovParSSN$distanceList[[i]]$Zs)
+        qrV = qr(V)
+        list(V = V, qrV = qrV, ViX = solve(qrV,
+          estCovParSSN$distanceList[[i]]$X), 
+          Viy = solve(qrV,estCovParSSN$distanceList[[i]]$y),
+          logdet = sum(log(abs(diag(qr.R(qrV))))),
+          XViX = crossprod(estCovParSSN$distanceList[[i]]$X,
+            solve(qrV,estCovParSSN$distanceList[[i]]$X)),
+          XViy = crossprod(estCovParSSN$distanceList[[i]]$y,
+            solve(qrV,estCovParSSN$distanceList[[i]]$X)),
+          yViy = crossprod(estCovParSSN$distanceList[[i]]$y,
+          solve(qrV,estCovParSSN$distanceList[[i]]$y)))
+    }
+
+    Sxx = Reduce('+',lapply(Vlist,function(x){x[['XViX']]}))
+    sxy = t(Reduce('+',lapply(Vlist,function(x){x[['XViy']]})))
+    syy = t(Reduce('+',lapply(Vlist,function(x){x[['yViy']]})))
+
+    betaHat = solve(Sxx,sxy)
+    
+    CijList <- foreach(i = 1:(nSubSamp -1)) %:% 
+		foreach(j = (i+1):nSubSamp) %dopar% {
+			makeSigijMats(DFr = DF, xy = estCovParSSN$xy, 
+			CorModels = CorModels, 
+			theta = estCovParSSN$estCovPar, 
+			addfunccol = estCovParSSN$mfcall[['addfunccol']],
+			subSampIndxCol = estCovParSSN$mfcall[['subSampIndxCol']], i, j,
+			distPath = eval(estCovParSSN$mfcall[['ssn.object']])@path, 
+			useTailDownWeight = FALSE, Vlist = Vlist)
+		}
+
+		CijList = unlist(CijList, recursive = FALSE)
+		Wxx = Reduce('+',lapply(CijList,function(x){x[['XViCViX']]}))
+
+		Sxxi = solve(Sxx)
+		covB = Sxxi + 2*Sxxi %*% Wxx %*% Sxxi
+
+    list(betaHat = betaHat, covB = covB, Vlist = Vlist)
+}
---
+++ R/subSampIndxSSN.R
@@ -0,0 +1,39 @@
+#-------------------------------------------------------------------------------
+#
+#           subSampIndxSSN
+#
+#-------------------------------------------------------------------------------
+
+#' creates a subsample index for a SpatialStreamNetwork object
+#'
+#' creates a subsample index for a SpatialStreamNetwork object so that 
+#'   it can be used with fast methods for big data
+#'
+#' @param ssn.object a SpatialStreamNetwork object, usually imported 
+#'   with SSN package
+#' @param nIndx the number of indexes (number of groups for subsampling)
+#'
+#' @return an object of class "SpatialStreamNetwork", which is the same 
+#'   as the input ssn.object, except the points data.frame within now
+#'   has a new field named subSampIndx which indexes the groups for 
+#'   subsampling. 
+#'
+#' @author Jay Ver Hoef
+#' @export
+
+subSampIndxSSN = function(ssn.object, nIndx)
+{
+  xy = ssn.object@obspoints@SSNPoints[[1]]@point.coords
+  xychar = paste0(xy[,1],xy[,2])
+  unik = !duplicated(xychar)  
+  xyunik = xy[seq_along(xychar)[unik],]
+  indx = rep(c(1:nIndx), times = ceiling(dim(xyunik)[1]/nIndx))[1:dim(xyunik)[1]]
+  indx = indx[order(runif(length(indx)))]
+  xyindx = merge(data.frame(xychar = xychar), 
+    data.frame(xychar = xychar[seq_along(xychar)[unik]], indx = indx),
+    by.x = "xychar", all.x = TRUE, sort = FALSE)
+  DF = getSSNdata.frame(ssn.object)
+  DF$subSampIndx = xyindx$indx
+  ssn.object = putSSNdata.frame(DF, ssn.object)
+  ssn.object
+}
