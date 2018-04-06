d67f06d12e5046c281d8941772152ed0da1406df
Erin Peterson <erin.peterson@qut.edu.au>
2018-04-06 12:00:03 +1000

    modified code so that when o.write = FALSE and distance matrices exist, it will skip over that step and move onto the next set of obs x obs, preds x obs, or preds x preds. Makes it more computationally efficient - especially when there are multiple sets of prediction sites

--- R/createBigDistMat.R
+++ R/createBigDistMat.R
@@ -143,74 +143,62 @@ createBigDistMat <- function(ssn, predpts = NULL, o.write = FALSE, amongpreds = 
       net.name <- paste("net", net.num, sep = "")
 
 
-
+      ##################################################################
       ## If observed sites > 0 #########################################
       if (site.no > 0) {
 
+          workspace.name1 <- paste(obs.path, "/dist.net", net.num, sep = "")
+
           ## get sorted pids to use as row and column names
-	  obs.pids<- sort(as.numeric(rownames(ssn@obspoints@SSNPoints[[1]]@network.point.coords[ind.obs,])))
+          obs.pids<- sort(as.numeric(rownames(ssn@obspoints@SSNPoints[[1]]@network.point.coords[ind.obs,])))
 
           ## Extract binaryID table
           bin.table <- dbReadTable(connect, net.name)
 
-	  workspace.name1 <- paste(obs.path, "/dist.net", net.num, sep = "")
+          exists <- file.exists(paste0(workspace.name1, ".bmat"))
 
-	  ## if(!o.write) {
-	  ##       exists <- file.exists(file.path(ssn@path, "distance", "obs", workspace.name1))
-	  ##       if (!missing(predpts) && !is.null(predpts))
-	  ##       {
-          ##           exists <- c(exists, file.exists(file.path(ssn@path,
-          ##                       "distance", predpts, workspace.name.a)),
-          ##                       file.exists(file.path(ssn@path, "distance",
-          ##                       predpts, workspace.name.b)))
-	  ##       }
-	  ##       ##If they're all already there, warn but continue
-	  ##       if(all(exists))
-	  ##       {
-	  ##       	if(!warned.overwrite) {
-	  ##       	    warned.overwrite <- TRUE
-	  ##       	    cat("Distance matrices already existed while o.write was set to FALSE. Not overwriting existing matrices\n")}
-	  ##       	next
-	  ##       }
-	  ##       ##if some exist but some don't that's an error
-	  ##       else if(any(exists) && any(!exists)) {
-	  ##       	stop("o.write was set to FALSE and some (but not all) distance matrices already existed")}
-	  ## }
+          ###################################################################
+          ## If o.write = FALSE and obs file exists
+	  if(!o.write & exists) {
+                   cat("Distance matrices for observed versus observed sites in network", net.num, "already exists and o.write = FALSE. Not overwriting existing matrices\n\n")
+
+          } else {
 
           ######################################################################
-          ## Create empty obs distance matrix
-          current_distance_matrix <-
-                fm.create(filenamebase = workspace.name1,
-                          nrow = site.no, ncol = site.no, type = "double")
+
+              ## Create empty obs distance matrix
+              current_distance_matrix <-
+                    fm.create(filenamebase = workspace.name1,
+                              nrow = site.no, ncol = site.no, type = "double")
 
 
-           rownames(current_distance_matrix) <- as.character(obs.pids)
-           colnames(current_distance_matrix) <- as.character(obs.pids)
+               rownames(current_distance_matrix) <- as.character(obs.pids)
+               colnames(current_distance_matrix) <- as.character(obs.pids)
 
-          ##Calculate distances between observed sites
-          if (is.null(getDoParName())) {
-              registerDoSEQ() # A little hack to avoid the foreach warning 1st time.
+              ##Calculate distances between observed sites
+              if (is.null(getDoParName())) {
+                  registerDoSEQ() # A little hack to avoid the foreach warning 1st time.
+              }
+
+              ##cl <- makeCluster(no.cores)
+              ##registerDoParallel(cl)
+              ## registerDoParallel(cores=no.cores)
+
+              ## Create vector iterator
+              itCol <- isplitVector(obs.pids, chunks = no.cores)
+
+              ans1<- foreach(obs.pids.vec=itCol,.packages = c("SSN", "filematrix","itertools","iterators"),
+                                 .errorhandling = "pass") %dopar% {
+
+                          amongObsBigDistMat(ssn = ssn, net.num = net.num, pids = obs.pids.vec,
+                                bin.table = bin.table, workspace.name = workspace.name1)
+                          finished1 <- TRUE
+                          return(finished1)
+                      }
+
+              ## stopCluster(cl)
+              ## registerDoSEQ()
           }
-
-          ##cl <- makeCluster(no.cores)
-          ##registerDoParallel(cl)
-          ## registerDoParallel(cores=no.cores)
-
-          ## Create vector iterator
-          itCol <- isplitVector(obs.pids, chunks = no.cores)
-
-          ans1<- foreach(obs.pids.vec=itCol,.packages = c("SSN", "filematrix","itertools","iterators"),
-                             .errorhandling = "pass") %dopar% {
-
-                      amongObsBigDistMat(ssn = ssn, net.num = net.num, pids = obs.pids.vec,
-                            bin.table = bin.table, workspace.name = workspace.name1)
-                      finished1 <- TRUE
-                      return(finished1)
-                  }
-
-          ## stopCluster(cl)
-          ## registerDoSEQ()
-
       } ## END IF SITE.NO > 0
 
 ##################################################################################
@@ -223,11 +211,23 @@ createBigDistMat <- function(ssn, predpts = NULL, o.write = FALSE, amongpreds = 
          #if(pred.site.no > 0) {
 
              ## Get pred pids
-             pred.pids<- sort(as.numeric(rownames(ssn@predpoints@SSNPoints[[pred.num]]@network.point.coords[ind.preds,])))
+         pred.pids<- sort(as.numeric(rownames(ssn@predpoints@SSNPoints[[pred.num]]@network.point.coords[ind.preds,])))
 
-             ## create empty distance matrices for obs-preds
-             workspace.name.a <- paste(preds.path, "/dist.net", net.num, ".a", sep = "")
-             workspace.name.b <- paste(preds.path, "/dist.net", net.num, ".b", sep = "")
+         ## create empty distance matrices for obs-preds
+         workspace.name.a <- paste(preds.path, "/dist.net", net.num, ".a", sep = "")
+         workspace.name.b <- paste(preds.path, "/dist.net", net.num, ".b", sep = "")
+
+         exists.a <- file.exists(paste0(workspace.name.a, ".bmat"))
+         exists.b <- file.exists(paste0(workspace.name.b, ".bmat"))
+
+###################################################################
+         ## If o.write = FALSE and obs file exists
+	 if(!o.write & all(exists.a, exists.b)) {
+             cat("Distance matrices for observed versus predicted sites in network",
+                 net.num, " already exist and o.write = FALSE.Not overwriting existing matrices\n\n")
+
+          } else {
+#####################
 
              current_distance_matrix_a <-
                  fm.create(filenamebase = paste0(workspace.name.a),
@@ -261,27 +261,38 @@ createBigDistMat <- function(ssn, predpts = NULL, o.write = FALSE, amongpreds = 
 
              ## stopCluster(cl)
              ## registerDoSEQ()
-
-
-      } ## END OBS-PRED SITE DISTANCES
+         }
+      }## END OBS-PRED SITE DISTANCES
 
 ##------------------------------------------------------
 ## ###############################################################################################
 
           ## Calculate distances between prediction sites ------------------------------------
-          if (amongpreds & pred.num > 0) {
+      if (amongpreds & pred.num > 0) {
 
-               pred.pids<- sort(as.numeric(rownames(ssn@predpoints@SSNPoints[[pred.num]]@network.point.coords[ind.preds,])))
-               net.name <- paste("net", net.num, sep = "")
-               bin.table <- dbReadTable(connect, net.name)
+           pred.pids<- sort(as.numeric(rownames(ssn@predpoints@SSNPoints[[pred.num]]@network.point.coords[ind.preds,])))
+           net.name <- paste("net", net.num, sep = "")
 
-               among.path <- paste0(preds.path, "/dist.net", net.num)
+           among.path <- paste0(preds.path, "/dist.net", net.num)
+
+           exists2 <- file.exists(paste0(among.path, ".bmat"))
+
+###################################################################
+           ## If o.write = FALSE and preds versus preds file exists
+	   if(!o.write & exists2) {
+             cat("Distance matrices for predicted versus predicted sites in network",
+                 net.num, " already exist and o.write = FALSE. Not overwriting existing matrices\n\n")
+
+           } else {
+#####################
+
+
                among_distance_matrix <- fm.create(filenamebase =among.path,
                      nrow = pred.site.no, ncol = pred.site.no, type = "double")
                rownames(among_distance_matrix) <- as.character(pred.pids)
                colnames(among_distance_matrix) <- as.character(pred.pids)
 
-
+               bin.table <- dbReadTable(connect, net.name)
                ##Calculate distances between observed sites
                ## cl <- makeCluster(no.cores)
                ## registerDoParallel(cl)
@@ -302,7 +313,8 @@ createBigDistMat <- function(ssn, predpts = NULL, o.write = FALSE, amongpreds = 
                ## stopCluster(cl)
                ## registerDoSEQ()
 
-      }
+           }
+      }## End among preds
   }
   stopCluster(cl)
   registerDoSEQ()
